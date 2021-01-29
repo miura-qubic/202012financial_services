341ff7a78568a7e75e712f1924e2fe30277d5908
miura <miura@qu-bic.jp>
2021-01-29 15:37:31 +0900

    トップ：ヘッダー、MV修正

--- js/myscript.js
+++ js/myscript.js
@@ -9,26 +9,36 @@ const tabletwidth = 1025;
 	const openMenu = document.getElementById('menu_open');
 	const Nav = document.querySelector('header nav');
 	const body = document.querySelector('body');
+	const header = document.querySelector('header');
 
 	openMenu.addEventListener('click', function () {
 		openMenu.classList.toggle('active');
 		Nav.classList.toggle('active');
 		body.classList.toggle('active');
+		header.classList.toggle('open');
 	});
 }
 
 
 $(function () {
-	$("body").removeClass("preload");
 	// JQueryの範囲
 
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 500){
+			$('.header_in').addClass('active');
+		}else{
+			$('.header_in').removeClass('active');
+		}
+	});
+	
 	// フェードイン
 	$('.fadein, .fadein02, .fadein03, .fadedown01, .fadedown02, .fadedown03, .fadedown04, .fadedown05, .fadedown06, .fadeleft, .faderight, .deco, .deco02, .view').on('inview',function(){
 		$(this).addClass('active');
 	});
-
-
-
-
+	
+	
+	
+	
+	$("body").removeClass("preload");
 
 });
